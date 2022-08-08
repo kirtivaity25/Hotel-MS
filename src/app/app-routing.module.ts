b4bed7805546f31c7e5e8bab197716926fd00b23
kirtivaity25 <kiivaity2520@gmail.com>
2022-08-08 23:42:36 +0530

    Dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,44 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'dashboard',
+    loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule)
+
+  },
+  {
+    path: 'customer',
+    loadChildren: () => import('./customer/customer.module').then(m => m.CustomerModule)
+
+  },
+  {
+    path: 'staff',
+    loadChildren: () => import('./staff/staff.module').then(m => m.StaffModule)
+
+  },
+  {
+    path: 'menu',
+    loadChildren: () => import('./menu/menu.module').then(m => m.MenuModule)
+
+  },
+  {
+    path: 'tables',
+    loadChildren: () => import('./tables/tables.module').then(m => m.TablesModule)
+
+  },
+  {
+    path: 'order',
+    loadChildren: () => import('./order/order.module').then(m => m.OrderModule)
+
+  } ,
+  {
+    path :'',
+    redirectTo :'/dashboard',
+    pathMatch : 'full'
+  }
+ 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
